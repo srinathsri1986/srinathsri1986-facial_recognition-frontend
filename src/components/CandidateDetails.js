61b5b11e4b18027671e8df27096ec1e4b9f26440
srinathsri1986 <srinath.srinivasan@acldigital.com>
2025-03-25 18:37:27 +0530

    Update CandidateDetails.js
    removal toast

--- src/components/CandidateDetails.js
+++ src/components/CandidateDetails.js
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-toast.configure();
 
 const CandidateDetails = ({ onClose = () => {}, onDetailsUpdated = () => {} }) => {
   const [firstName, setFirstName] = useState("");
